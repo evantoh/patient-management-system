01d313f5c3a4edb1374c983323f1c5c828da8e6a
evantoh <evanmwenda@gmail.com>
2018-03-20 13:53:42 +0300

    added navbar to base.html

--- patient/urls.py
+++ patient/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

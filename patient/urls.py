6a0c19301ea63b86b72dbd9a5957fc07df7b7ff7
evantoh <evanmwenda@gmail.com>
2018-03-21 22:14:13 +0300

    created the edit profile template

--- patient/urls.py
+++ patient/urls.py
@@ -4,9 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.index,name = 'index'),
     url('^updatedoc',views.updatedoc,name='updatedoc'),
-    url('^addpatient',views.addpatient,name='addpatient')
+    url('^addpatient',views.addpatient,name='addpatient'),
+    url('^treatment',views.treatment,name='treatment'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- patient/views.py
+++ patient/views.py
@@ -5,25 +5,27 @@ from django.contrib.auth.models import User
 from .models import Doctor
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
+def index(request):
+    current_user = request.user
+    doctor = Doctor.objects.get()
+    return render(request, 'profile.html', {'doctor':doctor})
+
+@login_required(login_url='/accounts/login')    
 def welcome(request):
     return render(request,'welcome.html')
-
-@login_required(login_url='/accounts/login/')    
+@login_required(login_url='/accounts/login')
 def updatedoc(request):
-    # current_user = request.user
-    # username = current_user.username
     if request.method == 'POST':
         form = UpdateDocForm(request.POST,request.FILES)
         if form.is_valid():
             new_doc = form.save(commit = False)
-            # new_doc.user_id = current_user
             new_doc.save()
-            return redirect('welcome') 
+            return redirect('index') 
     else:
         form = UpdateDocForm()
     return render(request,'doctor/profile.html',{"form":form})
-
-@login_required(login_url='/accounts/login/')    
+@login_required(login_url='/accounts/login')
 def addpatient(request):
     # current_user = request.user
     # username = current_user.username
@@ -38,16 +40,18 @@ def addpatient(request):
         form = UpdateDocForm()
     return render(request,'patient/profile.html',{"form":form})
 
-
-
+@login_required(login_url='/accounts/login')
 def treatment(request):
-    current_user =request.user
+    # current_user =request.user
     if request.method == 'POST':
         form = TreatmentForm(request.POST, request.FILES)
         if form.is_valid():
             treatment = form.save(commit=False)
             treatment.save()
+            # return redirect('') 
     else:
         form = TreatmentForm()
     return render(request, 'treatment/treatment.html', { 'form':form})
+
+
 

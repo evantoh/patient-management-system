18df50e9829841043670cff222e77de1a1c2325d
evantoh <evanmwenda@gmail.com>
2018-03-21 15:43:41 +0300

    defined the urls to add new patient

--- patient/urls.py
+++ patient/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url('^updatedoc',views.updatedoc,name='docupdate')
+    url('^updatedoc',views.updatedoc,name='updatedoc'),
+    url('^addpatient',views.updatedoc,name='addpatient')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

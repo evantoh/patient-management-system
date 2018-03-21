f63b649a1a24db6155656c765ae5c80ff2edea40
evantoh <evanmwenda@gmail.com>
2018-03-21 15:26:21 +0300

    add patient model with all its properties

--- patient/models.py
+++ patient/models.py
@@ -19,3 +19,25 @@ class Doctor(models.Model):
 
     class Meta:
         ordering = ['first_name']
+
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    date_of_birth = models.DateField()
+    phone_number = models.IntegerField(default=254)
+    gender = models.CharField(max_length=6)
+    id_number = models.IntegerField(default=0)
+    status = models.CharField(max_length=8)
+    email = models.EmailField()
+    blood_group = models.CharField(max_length=5)
+    doctor = models.ForeignKey(Doctor, null=True)
+    profile_photo = models.ImageField(upload_to='patients_photo/', null=True)
+
+
+    def __str__(self):
+            return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
+

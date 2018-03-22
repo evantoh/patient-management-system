b3b90dbf5459ca872d14d37f2fe5846fbba2bf4d
evantoh <evanmwenda@gmail.com>
2018-03-23 00:56:57 +0300

    about to deploy

--- pms/settings.py
+++ pms/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,6 +88,7 @@ DATABASES = {
     'PASSWORD':'evans123',
     }
 }
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
